056c7f0c5851fb87b8b2fcc0481d75b784f4b45b
k-jogama <pg.kj50@gmail.com>
2021-05-07 11:24:20 +0900

    カート機能の追加

--- app/LineItem.php
+++ app/LineItem.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class LineItem extends Model
 {
+    protected $fillable = ['cart_id', 'product_id', 'quantity'];
+    
     public function carts()
     {
         return $this->belongsToMany(
